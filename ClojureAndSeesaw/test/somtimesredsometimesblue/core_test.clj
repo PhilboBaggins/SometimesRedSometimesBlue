ba56c3821f6aafa5ec15b2b5aa8d347cf6415451
Phil <PhilboBaggins@users.noreply.github.com>
2013-09-28 17:47:41 +1000

    Better Clojure entropy test

--- ClojureAndSeesaw/test/somtimesredsometimesblue/core_test.clj
+++ ClojureAndSeesaw/test/somtimesredsometimesblue/core_test.clj
@@ -3,12 +3,10 @@
             [somtimesredsometimesblue.core :refer :all]))
 
 (deftest entropy-test
-  (testing "Entropy test"
-    (dorun (for [i (range 0 5)]
-      (let [c (get-colour)]
-        (println c)
-        (if (= c "red")
-          (println "Red!")
-          (println "Blue!"))
-        (println "")
-        )))))
+  (testing "Entropy test. The get-colour function should return \"red\" about 50% of the time"
+    (let [iterations 1000 colourCount (count (filter #(= %1 "red") (repeatedly iterations get-colour)))]
+      (println "get-colour returned \"red\"" colourCount "times out of" iterations "iterations")
+      (is (> colourCount 475))
+      (is (< colourCount 525))
+    )))
+
