d3ff75305680b05f26dcc4a94f6d803177552bdb
Phil <PhilboBaggins@users.noreply.github.com>
2013-09-28 17:45:45 +1000

    Somtimes red, sometimes blue with Clojure

--- ClojureAndSeesaw/src/somtimesredsometimesblue/core.clj
+++ ClojureAndSeesaw/src/somtimesredsometimesblue/core.clj
@@ -1,9 +1,23 @@
 (ns somtimesredsometimesblue.core
-  (:gen-class))
+  (:gen-class)
+  (:use seesaw.core seesaw.color))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
+(defn create-window [colour]
+  (invoke-later
+    (-> (frame :title "Somtimes red sometimes blue",
+               :content (canvas :background (color colour)
+                                :size [600 :by 600])
+               :on-close :exit)
+     pack!
+     ;;toggle-full-screen!
+     show!)))
+
+(defn get-colour []
+  (if (< (rand 1) 0.5)
+    "red"
+    "blue"))
+
+(defn -main [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (create-window (get-colour)))
