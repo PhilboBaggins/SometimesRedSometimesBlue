8086e9cefb3f0034b8a00b2f8d75f195b6ae70ed
Phil <PhilboBaggins@users.noreply.github.com>
2016-01-08 18:37:00 +1100

    Write main.lua for Corona SDK project

--- CoronaSDK/main.lua
+++ CoronaSDK/main.lua
@@ -4,4 +4,36 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+local function SometimesRedSometimesBlue()
+    local zeroOrOne = math.round(math.random())
+
+    if zeroOrOne == 0 then
+        display.setDefault( "background", 0, 0, 1 ) -- Red
+    else
+        display.setDefault( "background", 1, 0, 0 ) -- Blue
+    end
+
+    -- Always return true in case this function is used as an event handler
+    return true
+end
+
+local function main()
+    math.randomseed(os.time())
+
+    display.setStatusBar(display.HiddenStatusBar)
+
+    Runtime:addEventListener('touch', SometimesRedSometimesBlue)
+    Runtime:addEventListener('system', function(event)
+        if event.type == "applicationStart" or
+           event.type == 'applicationResume' then
+            return SometimesRedSometimesBlue()
+        end
+    end)
+    Runtime:addEventListener("accelerometer", function(event)
+        if event.isShake then
+            return SometimesRedSometimesBlue()
+        end
+    end)
+end
+
+main()
