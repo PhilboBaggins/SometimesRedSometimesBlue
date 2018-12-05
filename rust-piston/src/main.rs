0465a173ced15bab931d737b2f62564f704fc81e
Phil <PhilboBaggins@users.noreply.github.com>
2018-12-06 01:48:48 +1100

    Implement rust-piston logic

--- rust-piston/src/main.rs
+++ rust-piston/src/main.rs
@@ -5,5 +5,19 @@ use piston_window::*;
 use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let colour = if rng.gen() {
+        [1.0, 0.0, 0.0, 1.0] // Red
+    } else {
+       [0.0, 0.0, 1.0, 1.0] // Blue
+    };
+
+    let mut window: PistonWindow =
+        WindowSettings::new("Hello Piston!", [640, 480])
+        .exit_on_esc(true).build().unwrap();
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |_context, graphics| {
+            clear(colour, graphics);
+        });
+    }
 }
